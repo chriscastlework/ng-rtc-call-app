8cbbbbbbda1bcdad1bd934e0a398f87f59a26ab8
chris castle <christopher.castle@cbre.com>
2020-11-13 21:05:34 +0000

    trying to get hostin working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBz5WSPPhVGRVYDcK9xF1d-7ha5FWkjiNs",
+    authDomain: "ngrtccallapp.firebaseapp.com",
+    databaseURL: "https://ngrtccallapp.firebaseio.com",
+    projectId: "ngrtccallapp",
+    storageBucket: "ngrtccallapp.appspot.com",
+    messagingSenderId: "921839281962",
+    appId: "1:921839281962:web:59c005a1b169ad7b28e053",
+  },
 };

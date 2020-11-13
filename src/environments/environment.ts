0b77764b4f0adf196b12d27beb67fb15a82ce10e
chris castle <christopher.castle@cbre.com>
2020-11-13 18:41:51 +0000

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCyzKauqAXLWFOJpXd3Ah8bdx0BDCdicVs',
-    authDomain: 'chat-5aa35.firebaseapp.com',
-    databaseURL: 'https://chat-5aa35.firebaseio.com',
-    projectId: 'chat-5aa35',
-    storageBucket: 'chat-5aa35.appspot.com',
-    messagingSenderId: '328802266059'
-  }
+    apiKey: "AIzaSyBz5WSPPhVGRVYDcK9xF1d-7ha5FWkjiNs",
+    authDomain: "ngrtccallapp.firebaseapp.com",
+    databaseURL: "https://ngrtccallapp.firebaseio.com",
+    projectId: "ngrtccallapp",
+    storageBucket: "ngrtccallapp.appspot.com",
+    messagingSenderId: "921839281962",
+    appId: "1:921839281962:web:59c005a1b169ad7b28e053",
+  },
 };
 
 /*
